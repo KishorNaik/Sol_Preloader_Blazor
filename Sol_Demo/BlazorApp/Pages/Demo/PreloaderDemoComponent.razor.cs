b2e4f7d251f2831ae3a98bfd24597705a77a75eb
Kishor Naik <kishor.naik011.net@outlook.com>
2020-10-03 12:50:58 +0530

    Code Commit

---
+++ Sol_Demo/BlazorApp/Pages/Demo/PreloaderDemoComponent.razor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Pages.Demo
+{
+    public partial class PreloaderDemoComponent
+    {
+        #region Private Property
+
+        private bool IsLoad { get; set; }
+
+        #endregion Private Property
+
+        #region Protected Member
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                // Long Running Process
+                await Task.Delay(8000);
+
+                IsLoad = true;
+
+                base.StateHasChanged();
+            }
+        }
+
+        #endregion Protected Member
+    }
+}
